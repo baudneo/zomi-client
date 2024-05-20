196aa964d6642b8c71a2ea95f31a185fa69be037
baudneo <baudneo@protonmail.com>
2024-05-20 16:24:20 -0600

    Change log message formatting

--- src/zomi_client/Log/__init__.py
+++ src/zomi_client/Log/__init__.py
@@ -2,8 +2,11 @@ import logging
 
 from .handlers import BufferedLogHandler
 
-CLIENT_LOGGER_NAME: str = "ML:Client"
+CLIENT_LOGGER_NAME: str = "zomi:C"
+# "%(asctime)s.%(msecs)04d %(name)s[%(process)s] %(levelname)s %(module)s:%(lineno)d -> %(message)s"
+fmt = "%(asctime)s.%(msecs)04d %(name)s %(levelname)s[%(module)s:%(lineno)d] -> %(message)s"
+date_fmt = "%m/%d/%y %H:%M:%S"
 CLIENT_LOG_FORMAT = logging.Formatter(
-    "'%(asctime)s.%(msecs)04d' %(name)s[%(process)s] %(levelname)s %(module)s:%(lineno)d -> %(message)s",
-    "%m/%d/%y %H:%M:%S",
+    fmt,
+    date_fmt,
 )
