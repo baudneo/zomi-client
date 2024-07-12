7a589c1c5d6a17782d930af50cd383900600eb04
baudneo <86508179+baudneo@users.noreply.github.com>
2024-07-12 14:56:11 -0600

    Fix logging formatter; erroneous '0'

--- src/zomi_client/Log/__init__.py
+++ src/zomi_client/Log/__init__.py
@@ -3,8 +3,8 @@ import logging
 from .handlers import BufferedLogHandler
 
 CLIENT_LOGGER_NAME: str = "zomi:C"
-# "%(asctime)s.%(msecs)04d %(name)s[%(process)s] %(levelname)s %(module)s:%(lineno)d -> %(message)s"
-fmt = "%(asctime)s.%(msecs)04d %(name)s %(levelname)s[%(module)s:%(lineno)d] -> %(message)s"
+# "%(asctime)s.%(msecs)d %(name)s[%(process)s] %(levelname)s %(module)s:%(lineno)d -> %(message)s"
+fmt = "%(asctime)s.%(msecs)d %(name)s %(levelname)s[%(module)s:%(lineno)d]> %(message)s"
 date_fmt = "%m/%d/%y %H:%M:%S"
 CLIENT_LOG_FORMAT = logging.Formatter(
     fmt,
